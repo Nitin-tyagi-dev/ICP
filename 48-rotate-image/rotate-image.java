c05c7a699934c364684c52040b19fa73335a1683
Nitin-tyagi-dev <mrnitintyagi666@gmail.com>
2025-09-29 11:03:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (26.61%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -4,25 +4,28 @@ class Solution {
         int[][] mat = new int[n][n];
 
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                mat[j][n-i-1] = matrix[i][j] ;
+            for(int j = i + 1 ; j < n ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
 
-        // for(int i = 0 ; i < n ; i++){
-        //     for(int j = 0 ; j < n ; j++){
-        //         System.out.print(mat[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-
-
-
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                matrix[i][j] = mat[i][j];
+            int start = 0 ;
+            int end = n - 1;
+            while(start < end){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp ;
+                start++;
+                end--;
             }
             
-        }        
+        }
+
+
+
+        
     }
 }
