41231d1529cde9e2224e7ce12020b45ca1952ae5
Nitin Tyagi <mrnitintyagi666@gmail.com>
2025-09-30 16:27:30 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (27.88%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,39 +2,34 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-
-
-        if(n1 > n2) return findMedianSortedArrays(nums2 , nums1);
-
-        int low = 0 , high = n1 ;
-        int left = (n1 + n2 + 1)/2 ;
-        int n = n1 + n2 ;
-
-        while(low <= high){
-            int mid1 = (low + high) >> 1 ;
-            int mid2 = left - mid1;
-
-            int l1 = Integer.MIN_VALUE ,  l2 = Integer.MIN_VALUE ;
-            int  r1 = Integer.MAX_VALUE ,  r2 = Integer.MAX_VALUE ;
-
-            if(mid1 < n1) r1 = nums1[mid1];
-            if(mid2 < n2) r2 = nums2[mid2];
-
-            if(mid1 - 1 >= 0) l1 = nums1[mid1 -1];
-            if(mid2 - 1 >= 0) l2 = nums2[mid2 -1];
-
-            if(l1 <= r2 && l2 <= r1){
-                if(n % 2 == 1 ) return Math.max(l1 , l2);
-                return ((double)(Math.max(l1,l2) + Math.min(r1 , r2))) / 2.0; 
+        int[] arr = new int[n1 + n2];
+        int i = 0 ;
+        int j = 0 ;
+        int k = 0 ;
+        while( i < n1 && j < n2){
+            if(nums1[i] >= nums2[j]){
+                arr[k++] = nums2[j++] ;
+            }else{
+                arr[k++] = nums1[i++];
             }
-
-            else if( l1 > r2) high = mid1 - 1 ;
-            else low = mid1 + 1;
-
         }
 
-        return 0 ;
+        while(i < n1){
+            arr[k++] = nums1[i++] ;
+        }
 
+        while(j < n2){
+            arr[k++] = nums2[j++] ;
+        }
 
+        if(arr.length % 2 == 1) {
+            return (double)(arr[arr.length / 2]);
+        }
+        else{
+            //System.out.println(arr[arr.length / 2]+" "+ (arr[arr.length / 2 - 1]));
+            double z = (double)(arr[arr.length / 2] + (arr[arr.length / 2 - 1])) / 2 ;
+            return z;
+        } 
+        
     }
 }
